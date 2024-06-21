ab184b274beba47bb912b915b462231e5b8fcb40
Erin Wolff <erin_wolff@outlook.com>
2024-06-21 13:55:38 -0700

    updated borderRadius & text input size

--- src/scenes/createScene.js
+++ src/scenes/createScene.js
@@ -39,23 +39,22 @@ export default class CreateScene extends Phaser.Scene {
       })
       .addBackground(this.rexUI.add.roundRectangle(0, 0, 0, 0, 0, 0xcc3a33));
 
-    const text = this.add.rexInputText(0, 0, 200, 50, {
+    const text = this.add.rexInputText(0, 0, 240, 50, {
       type: "text",
       placeholder: "Name Your Priest",
       fontSize: "20px",
       color: "#000000",
       backgroundColor: "#DD9292",
       fontFamily: "Lato",
-      borderRadius: 10,
-      border: 3,
-      maxLength: 16,
+      borderRadius: "10px",
+      maxLength: 20,
     });
 
     sidebar.add(text, 0, "center", {
       top: 100,
       bottom: 20,
-      left: 240,
-      right: 20,
+      left: 270,
+      right: 10,
     });
 
     sidebar.layout();

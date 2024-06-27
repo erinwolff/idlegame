0ef29cb394d48b5b6478ead3411a11be47b9f1c0
Erin Wolff <erin_wolff@outlook.com>
2024-06-27 14:03:36 -0700

    added constants for the various game phases, with placeholder text

---
+++ src/constants/upgrades.js
@@ -0,0 +1,83 @@
+const initiateUpgrades = [
+  {
+    name: "Prayer Efficiency",
+    description: "Placeholder text.",
+    cost: 10,
+  },
+  {
+    name: "Prayer Automation",
+    description: "Placeholder text.",
+    cost: 50,
+  },
+  {
+    name: "Basic Rituals",
+    description: "Placeholder text.",
+    cost: 30,
+  },
+  {
+    name: "Recruit Followers",
+    description: "Placeholder text.",
+    cost: 20,
+  },
+];
+
+const priestessUpgrades = [
+  {
+    name: "Potent Prayer Types",
+    description: "Placeholder text.",
+    cost: 100,
+  },
+  {
+    name: "Construct Places of Worship",
+    description: "Placeholder text.",
+    cost: 200,
+  },
+];
+
+const highPriestessUpgrades = [
+  {
+    name: "Rituals",
+    description: "Placeholder text.",
+    cost: 500,
+  },
+  {
+    name: "Recruitment",
+    description: "Ascend to godhood and gain unlimited faith.",
+    cost: 1000,
+  },
+];
+
+const oracleUpgrades = [
+  { name: "Divine Intervention", description: "Placeholder text.", cost: 1000 },
+  { name: "Conversions", description: "Placeholder text.", cost: 2000 },
+];
+
+const saintUpgrades = [
+  { name: "Relics & Artifacts", description: "Placeholder text.", cost: 5000 },
+];
+
+const demigodUpgrades = [
+  {
+    name: "Celestial Interventions",
+    description: "Placeholder text.",
+    cost: 10000,
+  },
+];
+
+const goddessUpgrades = [
+  {
+    name: "Transcendence",
+    description: "Placeholder text.",
+    cost: 100000,
+  },
+];
+
+export {
+  initiateUpgrades,
+  priestessUpgrades,
+  highPriestessUpgrades,
+  oracleUpgrades,
+  saintUpgrades,
+  demigodUpgrades,
+  goddessUpgrades,
+};

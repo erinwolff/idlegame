22116f1d7f7c55d6f11491521ec1b786eff3f780
Erin Wolff <erin_wolff@outlook.com>
2024-07-18 15:08:51 -0700

    dynamically updating sprites and backgrounds based on the current phase

--- src/scenes/createScene.js
+++ src/scenes/createScene.js
@@ -3,6 +3,7 @@ import Topbar from "../components/topbar";
 import { getCurrentPhaseUpgrades } from "../utils/getCurrentPhaseUpgrades";
 import { resetUpgradeButtons } from "../utils/resetUpgradeButtons";
 import { getCurrentPhase } from "../utils/getCurrentPhase";
+import { phaseData } from "../constants/phaseData";
 
 export default class CreateScene extends Phaser.Scene {
   constructor() {
@@ -11,41 +12,53 @@ export default class CreateScene extends Phaser.Scene {
   }
 
   create() {
+    this.totalFaith = 0;
+    this.currentPhase = getCurrentPhase(this.totalFaith); // Use your function
+
+    // Find the index of the initial phase in phaseData
+    this.currentPhaseIndex = phaseData.findIndex(
+      (phase) => phase.name === this.currentPhase
+    );
+
     let scaleX =
       this.cameras.main.width /
-      this.textures.get("acolyteBackground").getSourceImage().width;
+      this.textures
+        .get(phaseData[this.currentPhaseIndex].backgroundKey)
+        .getSourceImage().width;
     let scaleY =
       this.cameras.main.height /
-      this.textures.get("acolyteBackground").getSourceImage().height;
+      this.textures
+        .get(phaseData[this.currentPhaseIndex].backgroundKey)
+        .getSourceImage().height;
 
-    this.add
+    // Load initial background and acolyte images
+    this.background = this.add
       .image(
         this.cameras.main.centerX,
         this.cameras.main.centerY,
-        "acolyteBackground"
+        phaseData[this.currentPhaseIndex].backgroundKey
       )
       .setOrigin(0.5, 0.5)
       .setScale(scaleX, scaleY);
+
+    this.acolyte = this.add
+      .image(
+        this.cameras.main.centerX,
+        this.cameras.main.centerY,
+        phaseData[this.currentPhaseIndex].spritePhase
+      )
+      .setOrigin(0.5, 0.2) // Make sure to reset origin for the new images
+      .setScale(0.3)
+      .setDepth(5)
+      .setInteractive({ useHandCursor: true });
     this.cameras.main.setBackgroundColor("#ffffff");
     this.input.enabled = true;
 
-    this.totalFaith = 0;
-    this.currentPhase = getCurrentPhase(this.totalFaith);
     this.prayerAutomationFaithEvent = false; // passiveFaithEvent
     this.recruitFollowersFaithEvent = false;
     this.basicRitualsFaithEvent = false;
     this.totalFollowers = 0;
     this.playerName = "";
-
-    this.acolyte = this.add.image(
-      this.cameras.main.centerX,
-      this.cameras.main.centerY,
-      "acolyte"
-    );
-    this.acolyte.setOrigin(0.5, 0.2);
-    this.acolyte.setScale(0.3);
-    this.acolyte.setDepth(5);
-    this.acolyte.setInteractive({ useHandCursor: true });
 
     this.topbar = new Topbar(
       this,
@@ -69,7 +82,7 @@ export default class CreateScene extends Phaser.Scene {
     );
 
     this.acolyte.on("pointerdown", () => {
-      this.totalFaith += 1;
+      this.totalFaith += 10000;
       this.topbar.updateFaithLabel(this.totalFaith);
       this.playerName = this.sidebar.nameInput.text;
     });
@@ -79,6 +92,33 @@ export default class CreateScene extends Phaser.Scene {
     this.totalFaith = 0;
     this.totalFollowers = 0;
     this.currentPhase = getCurrentPhase(this.totalFaith);
+
+    this.currentPhaseIndex = phaseData.findIndex(
+      (phase) => phase.name === this.currentPhase
+    );
+    let scaleX =
+      this.cameras.main.width /
+      this.textures
+        .get(phaseData[this.currentPhaseIndex].backgroundKey)
+        .getSourceImage().width;
+    let scaleY =
+      this.cameras.main.height /
+      this.textures
+        .get(phaseData[this.currentPhaseIndex].backgroundKey)
+        .getSourceImage().height;
+    this.background
+      .setTexture(phaseData[this.currentPhaseIndex].backgroundKey)
+      .setOrigin(0.5, 0.5)
+      .setScale(scaleX, scaleY);
+    this.acolyte
+      .setTexture(phaseData[this.currentPhaseIndex].spritePhase)
+      .setOrigin(0.5, 0.2)
+      .setScale(0.3)
+      .setDepth(5)
+      .setInteractive({ useHandCursor: true });
+    this.cameras.main.setBackgroundColor("#ffffff");
+    this.input.enabled = true;
+
     if (this.topbar) {
       // Check if the topbar exists before updating it
       this.topbar.updateFaithLabel(this.totalFaith);
@@ -114,9 +154,39 @@ export default class CreateScene extends Phaser.Scene {
   // function to listen for totalFaith updates and update the currentPhase. Update the sidebar with the new phase and reset the upgrade buttons
   update() {
     const newPhase = getCurrentPhase(this.totalFaith);
+    const newPhaseIndex = phaseData.findIndex(
+      (phase) => phase.name === newPhase
+    );
+
     if (newPhase !== this.currentPhase) {
       console.log("Phase changed from: ", this.currentPhase, " to: ", newPhase);
+
+      this.currentPhaseIndex = newPhaseIndex;
       this.currentPhase = newPhase;
+
+      let scaleX =
+        this.cameras.main.width /
+        this.textures
+          .get(phaseData[this.currentPhaseIndex].backgroundKey)
+          .getSourceImage().width;
+      let scaleY =
+        this.cameras.main.height /
+        this.textures
+          .get(phaseData[this.currentPhaseIndex].backgroundKey)
+          .getSourceImage().height;
+      // Update background and acolyte images using keys from phaseData
+      this.background
+        .setTexture(phaseData[this.currentPhaseIndex].backgroundKey)
+        .setOrigin(0.5, 0.5)
+        .setScale(scaleX, scaleY);
+      this.acolyte
+        .setTexture(phaseData[this.currentPhaseIndex].spritePhase)
+        .setOrigin(0.5, 0.2)
+        .setScale(0.3)
+        .setDepth(5)
+        .setInteractive({ useHandCursor: true });
+      this.cameras.main.setBackgroundColor("#ffffff");
+      this.input.enabled = true;
       this.sidebar.currentPhaseLabel.text = this.currentPhase;
       this.sidebar.upgrades = getCurrentPhaseUpgrades(this.currentPhase);
       resetUpgradeButtons(this.currentPhase);

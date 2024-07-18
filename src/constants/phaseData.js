bba1850f66ef594a68eee6092c2876325a7b40d7
Erin Wolff <erin_wolff@outlook.com>
2024-07-18 15:08:13 -0700

    created phaseData array to dynamically update sprites and background images

---
+++ src/constants/phaseData.js
@@ -0,0 +1,39 @@
+const phaseData = [
+  {
+    name: "Initiate",
+    backgroundKey: "background_phase1",
+    spritePhase: "initiate",
+  },
+  {
+    name: "Priestess",
+    backgroundKey: "background_phase2",
+    spritePhase: "priestess",
+  },
+  // {
+  //   name: "High Priestess",
+  //   backgroundKey: "background_phase3",
+  //   spritePhase: "high_priestess",
+  // },
+  // {
+  //   name: "Oracle",
+  //   backgroundKey: "background_phase4",
+  //   spritePhase: "oracle",
+  // },
+  // {
+  //   name: "Saint",
+  //   backgroundKey: "background_phase5",
+  //   spritePhase: "saint",
+  // },
+  // {
+  //   name: "Demigod",
+  //   backgroundKey: "background_phase6",
+  //   spritePhase: "demigod",
+  // },
+  // {
+  //   name: "Goddess",
+  //   backgroundKey: "background_phase7",
+  //   spritePhase: "goddess",
+  // },
+];
+
+export { phaseData };
